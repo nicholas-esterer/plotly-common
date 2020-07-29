58b76cc5f267d5d35273d70ebd00b85451aa4df9
Nicholas Esterer <nicholas.esterer@plot.ly>
2020-07-29 16:24:08 -0400

    contiguous labels supported by label_to_colors
    This makes it faster

--- image_utils.py
+++ image_utils.py
@@ -2,12 +2,17 @@ import plotly.express as px
 import numpy as np
 import itertools
 
+
 def fromhex(n):
     return int(n, base=16)
 
 
 def label_to_colors(
-    img, colormap=px.colors.qualitative.Light24, alpha=128, color_class_offset=0
+    img,
+    colormap=px.colors.qualitative.Light24,
+    alpha=128,
+    color_class_offset=0,
+    labels_contiguous=False,
 ):
     """
     Take a tensor containing integers representing labels and return an dim0x...dim(D-1)x4
@@ -20,28 +25,37 @@ def label_to_colors(
     use of a particular range of colors in the colormap. This is useful for
     example if 0 means 'no class' but we want the color of class 1 to be
     colormap[0].
+    If labels in img are a contiguous set of integers, then, for speed,
+    labels_contiguous=True can be set and this will iterate from the smallest
+    through the greatest label, otherwise this iterates through the set of
+    labels as determined from img.
     """
     colormap = [
         tuple([fromhex(h[s : s + 2]) for s in range(0, len(h), 2)])
         for h in [c.replace("#", "") for c in colormap]
     ]
     if type(alpha) is not type(list()):
-        alpha=[alpha]
-    cm_alpha=list(zip(colormap,itertools.cycle(alpha)))
+        alpha = [alpha]
+    cm_alpha = list(zip(colormap, itertools.cycle(alpha)))
     cimg = np.zeros(img.shape + (3,), dtype="uint8")
     alpha = np.zeros(img.shape + (1,), dtype="uint8")
-    for c in set(img.flatten()):
-        cimg[img == c],alpha[img == c] = cm_alpha[(c + color_class_offset) % len(colormap)]
-    return np.concatenate(
-        (cimg, alpha), axis=len(cimg.shape)-1
-    )
+    if labels_contiguous:
+        labels = range(img.min(), img.max() + 1)
+    else:
+        labels = set(img.flatten())
+    for c in labels:
+        cimg[img == c], alpha[img == c] = cm_alpha[
+            (c + color_class_offset) % len(colormap)
+        ]
+    return np.concatenate((cimg, alpha), axis=len(cimg.shape) - 1)
+
 
 def combine_last_dim(
     img,
     output_n_dims=3,
-    combiner=lambda x: (np.sum(np.abs(x),axis=-1)!=0).astype('float')
+    combiner=lambda x: (np.sum(np.abs(x), axis=-1) != 0).astype("float"),
 ):
-    if len(img.shape)==output_n_dims:
+    if len(img.shape) == output_n_dims:
         return img
-    imgout=combiner(img)
+    imgout = combiner(img)
     return imgout
